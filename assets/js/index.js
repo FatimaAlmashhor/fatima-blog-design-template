c2bfcc92318506095bad5c66cdb3dfe449b2c88b
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-12-03 18:41:28 +0300

    Refactor : language mask

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 // const circle = document.querySelector('.animat_circle');
-
+// this is for circle animation
 const circleLetters = () => {
     const circle = document.querySelector('.animat_circle');
     const text = circle.innerHTML;// Note I am being lazy here and assuming the string has no unwanted whitespace
@@ -11,5 +11,22 @@ const circleLetters = () => {
 
 }
 
+// this is for the language section hoveing 
+const lang_sec = document.querySelector('.lang_sec');
+lang_sec.addEventListener('mouseover', (e) => {
+    lang_sec.style.setProperty('--a', `400px`)
+    e.target.addEventListener('mousemove', (e) => {
+        const { clientX, clientY } = e
+        const x = Math.round((clientX / window.innerWidth) * 100)
+        const y = Math.round((clientY / window.innerHeight) * 100)
+
+        lang_sec.style.setProperty('--x', `${x}%`)
+        lang_sec.style.setProperty('--y', `${y}%`)
+        console.log(lang_sec)
+    })
+});
+lang_sec.addEventListener('mouseleave', (e) => {
+    lang_sec.style.setProperty('--a', `0px`)
+});
 
 circleLetters();
